7cf8a3b7daa304dcc95ca71dc114e0f226c2f7be
Riddhi2002 <80107241+Riddhi2002@users.noreply.github.com>
2023-06-26 18:51:53 +0530

    Time: 23 ms (86.95%), Space: 9.8 MB (56.97%) - LeetHub

---
+++ 0077-combinations/0077-combinations.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+vector<vector<int>> res;
+vector<int> temp;
+    vector<vector<int>> combine(int n, int k) {
+        helper(n,k,1);
+        return res;
+    }
+    void helper(int n, int k, int i){
+        if(temp.size()==k){
+            res.push_back(temp);
+            return;
+        }
+        for(int j=i; j<=n; j++){
+            temp.push_back(j);
+            helper(n, k, j+1);
+            temp.pop_back();
+        }
+    }
+};
