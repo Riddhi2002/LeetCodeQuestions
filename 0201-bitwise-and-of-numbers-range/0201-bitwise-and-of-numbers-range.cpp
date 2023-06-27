6e20d4dadc31f424f6643b4b9d66b74e5f25b347
Riddhi2002 <80107241+Riddhi2002@users.noreply.github.com>
2023-06-27 15:52:08 +0530

    Time: 7 ms (91.17%), Space: 5.9 MB (94.85%) - LeetHub

---
+++ 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int rangeBitwiseAnd(int left, int right) {
+        int ans=0;
+        for(int i=31; i>=0; i--){
+            if((left&(1<<i))!=(right&(1<<i))){
+                break;
+            }
+            else{
+                ans|=(left&(1<<i));
+            }
+        }
+        return ans;
+    }
+};
